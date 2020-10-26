87eaa11a9b72c2b0a67672e95e05b34a08871d14
Garrett Breach <breachg1992@gmail.com>
2020-10-26 19:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (musicians, instruments) {
   var emptyArray = []
-  for (musicians,instruments=0; musicians,instruments< 4; musicians++){
+  for (musicians=0; musicians< 4; musicians++){
     emptyArray.push(`${musicians} plays ${instruments}`)
   }
   return emptyArray
